a1a317b48375800a83bc7a6eb95e26741f207c53
Abdul Samad Sheikh <abdulsamadsjc2002@gmail.com>
2025-03-23 08:56:26 +0000

    Update

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1742688697',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
