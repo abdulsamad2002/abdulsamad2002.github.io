c361c943e5660f51391806ec4e209df62579f85e
Abdul Samad Sheikh <abdulsamadsjc2002@gmail.com>
2025-04-07 19:50:02 +0000

    Update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744054497',resources: [
+    cacheName: 'chirpy-1744055304',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
